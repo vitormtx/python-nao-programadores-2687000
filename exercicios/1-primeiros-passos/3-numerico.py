a8fd36b769fbc0147cc1a61f7b365cfd84c6e8d5
vitormtx <vtrmteixeira@gmail.com>
2024-08-14 19:16:37 +0000

    resolução exercicio numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,14 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print (type(data_nascimento))
+print (type (idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+triplica_nome = 'vitor' * 3
+print(triplica_nome)
 
 # Realize uma operação entre dados do tipo inteiro e float
+dobrapi = 2*3.14
+print(dobrapi)
