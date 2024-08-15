4be7859225254ff58b3309244712d2bbd3d4d78b
vitormtx <vtrmteixeira@gmail.com>
2024-08-15 19:39:41 +0000

    Resolução exercício avaliação cursos

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -18,13 +18,13 @@ nota_cursos = {}
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
-if curso1 in(lista_cursos):
+if curso1 in lista_cursos:
   nota_curso1 = input(f"Dê a nota para o curso {curso1}: ")
   nota_cursos[curso1] = int(nota_curso1)
-if curso2 in(lista_cursos):
+if curso2 in lista_cursos:
   nota_curso2 = input(f"Dê a nota para o curso {curso2}: ")
   nota_cursos[curso2] = int(nota_curso2)
-if curso3 in(lista_cursos):
+if curso3 in lista_cursos:
   nota_curso3 = input(f"Dê a nota para o curso {curso3}: ")
   nota_cursos[curso3] = int(nota_curso3) 
 else:
