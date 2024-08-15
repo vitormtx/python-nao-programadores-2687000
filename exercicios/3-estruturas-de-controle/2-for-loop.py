1d66da2b03bc7bd9da137554418bf4a990681755
vitormtx <vtrmteixeira@gmail.com>
2024-08-15 21:19:51 +0000

    Resolução exercicio for-loopy

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,16 @@
 # Crie uma lista
+lista = [1,'a',2,'b',3,'c',4,'d',5,'e',6,'f']
 
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for elemento in lista:
+  print(elemento)
 
 # Crie um objeto iterável utilizando a função range()
+objit = range(10)
+print(objit)
+print(list(objit))
 
-
-# Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+# Use esse objeto iterável para criar um for loop que imprima na tela a frase 
+# "Estou aprendendo uma linguagem de programação."
+for indice in objit:
+  print(f"{indice} - Estou aprendendo uma linguagem de programação.")
